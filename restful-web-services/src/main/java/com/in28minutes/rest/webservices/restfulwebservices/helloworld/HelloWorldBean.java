cc12013e05280ec2adee1e70130500f625d57600
Alok-Blueocean <alokrnjn3@gmail.com>
2019-11-12 17:35:17 +0530

    add

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -7,11 +7,9 @@ public class HelloWorldBean {
 	public HelloWorldBean(String message) {
 		this.message = message;
 	}
-
 	public String getMessage() {
 		return message;
 	}
-
 	public void setMessage(String message) {
 		this.message = message;
 	}
